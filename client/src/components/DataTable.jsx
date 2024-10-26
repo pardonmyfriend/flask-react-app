e3932aaf78fdcce7defaaf7576b2cdd3ea599d3b
Zofia Sitek <zofia.a.sitek@gmail.com>
2024-10-26 13:11:17 +0200

    New style for DataGrid

--- client/src/components/DataTable.jsx
+++ client/src/components/DataTable.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { DataGrid } from '@mui/x-data-grid';
-import { Box } from '@mui/material';
-import FileUploader from './FileUploader';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { Box } from '@mui/material';  
 
 const DataTable = ({ data, setActiveStepFulfilled }) => {
 
@@ -21,7 +20,7 @@ const DataTable = ({ data, setActiveStepFulfilled }) => {
                     loading={!data.rows.length}
                     pageSize={10}
                     rowsPerPageOptions={[10, 20, 50]}
-                    density='compact'
+                    //density='compact'
                     showCellVerticalBorder = 'true'
                     showColumnVerticalBorder = 'true'
                     checkboxSelection = 'true'
@@ -30,14 +29,43 @@ const DataTable = ({ data, setActiveStepFulfilled }) => {
                           fontWeight: 'bold', 
                           fontSize: '17px',
                         },
-                        // '& .MuiDataGrid-columnHeaderTitleContainer': {
-                        //   backgroundColor: 'red', 
-                        // },
-                        // '& .MuiDataGrid-topContainer': {
-                        //   backgroundColor: 'red', 
-                        // },
                         '& .MuiDataGrid-row:nth-of-type(2n)': {
-                            backgroundColor: '  #eefdfd  ', 
+                            backgroundColor: '    #f6f6f6  ', 
+                        },
+                        '& .MuiDataGrid-toolbar': {
+                            color: 'white', // Dodatkowe ustawienia dla kolorów w DataGrid
+                        },
+                        '& .MuiButton-textPrimary': {
+                            color: 'white !important', 
+                        },
+                        '& .MuiTypography-root': {
+                            color: 'white !important', // Ustaw kolor tekstu na biały dla typografii
+                        },
+                        '& .MuiButtonBase-root': {
+                            color: 'white !important', // Ustaw kolor tekstu na biały dla przycisków
+                        },
+                        '& .MuiSvgIcon-root' : {
+                            color: '  #3fbdbd !important',
+                        },
+                        '& .MuiDataGrid-columnsManagement' : {
+                            backgroundColor: '  #3fbdbd !important',
+                        },
+                      }}
+                      slots={{
+                        toolbar: GridToolbar,
+                      }}
+                      slotProps={{
+                        toolbar: {
+                          sx: {
+                            backgroundColor: '   #474747', // Przykładowy kolor tła
+                            fontWeight: 'bold', // Pogrubienie tekstu
+                            padding: '10px', // Dodatkowe odstępy
+                            fontSize: '30px',
+                            color: ' #ffffff',
+                            '& .MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeSmall MuiButton-textSizeSmall MuiButton-colorPrimary MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeSmall MuiButton-textSizeSmall MuiButton-colorPrimary css-8iab9z-MuiButtonBase-root-MuiButton-root': { // Stylizacja typografii w toolbarze
+                            color: 'white', // Ustaw kolor tekstu na biały
+                            }
+                          },
                         },
                       }}
                 />
